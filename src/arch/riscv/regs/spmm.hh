bf7f1911b02a624c1e00bd7bac84a6b2327b4373
xixi <xj13992157229@outlook.com>
2025-08-06 09:51:59 +0800

    ext-spmm: support fp32 (with MERGE bugs).

--- src/arch/riscv/regs/spmm.hh
+++ src/arch/riscv/regs/spmm.hh
@@ -21,17 +21,18 @@ using spmm_reg_t = SpMMRegContainer;
 
 
 const int NumSpMMStandardRegs = 32;
-const int NumSpMMRegs = NumSpMMStandardRegs;
+const int NumSpMMInternalRegs = 1; // Used by spmm buffer
+const int NumSpMMRegs = NumSpMMStandardRegs + NumSpMMInternalRegs;
 
 const std::vector<std::string> SpMMRegNames = {
     "v0",   "v1",   "v2",   "v3",   "v4",   "v5",   "v6",   "v7",
     "v8",   "v9",   "v10",  "v11",  "v12",  "v13",  "v14",  "v15",
     "v16",  "v17",  "v18",  "v19",  "v20",  "v21",  "v22",  "v23",
     "v24",  "v25",  "v26",  "v27",  "v28",  "v29",  "v30",  "v31",
+    "vbuf"
 };
 
-// // vector index
-// const int VecMemInternalReg0 = NumVecStandardRegs;
+const int SpMMBufFlagReg = NumSpMMStandardRegs;
 
 static inline TypedRegClassOps<RiscvISA::SpMMRegContainer> spmmRegClassOps;
 
